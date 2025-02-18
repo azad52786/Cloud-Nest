36a4e4a90835df1ffc0a2769975f242b2692d273
Azad <arambagh53@gmail.com>
2025-02-18 18:58:21 +0530

    auth page created

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,8 +1,148 @@
-import React from "react";
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { montserrat } from "@/app/(auth)/layout";
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 type FormType = "sign-in" | "sign-up";
+
+const authFormSchema = (formType: FormType) => {
+  return z.object({
+    email: z.string().email(),
+    name:
+      formType === "sign-up"
+        ? z.string().min(2).max(50)
+        : z.string().optional(),
+    password: z.string().min(8).max(20),
+  });
+};
+
 const AuthForm = ({ type }: { type: FormType }) => {
-  return <div></div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const formSchema = authFormSchema(type);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+  return (
+    <div className="flex-grow flex items-center justify-center p-3 min-h-[600px] relative">
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div>
+      </div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className={`${montserrat.className} auth-form `}
+        >
+          <h1 className={`form-title`}>
+            {type === "sign-in" ? "Sign In" : "Sign Up"}
+          </h1>
+          <div className=" shadow-lg pt-6 shadow-purple-400 flex flex-col gap-5 p-5  rounded-md ">
+            {type === "sign-up" && (
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className=" flex flex-col gap-y-2">
+                      <FormLabel>Name</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Enter your name"
+                          className="border border-purple-500 border-b-2 border-r-2 outline-none"
+                          {...field}
+                        />
+                      </FormControl>
+
+                      <FormMessage />
+                    </div>
+                  </FormItem>
+                )}
+              />
+            )}
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <div className=" flex flex-col gap-2">
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter your email"
+                        className="border border-purple-500 border-b-2 border-r-2 outline-none"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </div>
+                </FormItem>
+              )}
+            />
+            <Button
+              size={"lg"}
+              variant={"myButton"}
+              className={`${montserrat.className} w-full text-gray-200 `}
+              type="submit"
+            >
+              {type === "sign-in" ? "Sign In" : "Sign Up"}
+              {isLoading && (
+                <Image
+                  src="/assets/icons/loader.svg"
+                  alt="loader"
+                  width={24}
+                  height={24}
+                  className="ml-2 animate-spin"
+                />
+              )}
+            </Button>
+            <div className=" body-2 flex justify-center">
+              {type === "sign-in" ? (
+                <div>
+                  Don't have an account?{" "}
+                  <Link href="sign-up" className="font-medium underline">
+                    Sign Up
+                  </Link>
+                </div>
+              ) : (
+                <div>
+                  Already have an account?{" "}
+                  <Link href="/sign-in" className="font-medium underline">
+                    Sign In
+                  </Link>
+                </div>
+              )}
+            </div>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
 };
 
 export default AuthForm;
