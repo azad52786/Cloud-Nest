eb089a81b3492cdc700c639ccc9813163e320523
Azad <arambagh53@gmail.com>
2025-02-20 23:05:22 +0530

    slider done

---
+++ components/Sidebar.tsx
@@ -0,0 +1,95 @@
+"use client";
+import { montserrat } from "@/app/(auth)/layout";
+import { avatarPlaceholderUrl, navItems } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const Sidebar = () => {
+  const pathname = usePathname();
+  return (
+    <aside
+      className={`${montserrat.className} remove-scrollbar hidden h-screen w-[90px] flex-col gap-5 overflow-auto px-5 py-7 sm:flex lg:w-[280px] xl:w-[325px] bg-slate-700`}
+    >
+      <Image
+        src="/assets/icons/logo-full-brand.svg"
+        alt="logo"
+        width={160}
+        height={50}
+        className=" h-auto hidden lg:block"
+      />
+
+      <Image
+        src="/assets/icons/logo-brand.svg"
+        alt="logo"
+        width={52}
+        height={52}
+        className="lg:hidden"
+      />
+
+      <nav
+        className=" text-xl font-bold text-white  gap-1 grow-[1] flex-1
+ "
+      >
+        <ul className=" flex flex-1 flex-col gap-3">
+          {navItems.map(({ name, url, icon }) => {
+            return (
+              <Link key={name} href={url} className="lg:w-full">
+                <li
+                  className={cn(
+                    "flex gap-5 rounded-xl lg:w-full justify-center lg:justify-start items-center lg:px-[30px] h-[54px] lg:rounded-full ",
+                    pathname === url &&
+                      "bg-sky-500 text-white shadow-blue-500 shadow-lg",
+                    pathname !== url && "hover:shadow-md hover:shadow-sky-400"
+                  )}
+                >
+                  <Image
+                    src={icon}
+                    alt={name}
+                    width={24}
+                    height={24}
+                    className={cn(
+                      " w-6 invert-0 opacity-35",
+                      pathname === url && " opacity-100"
+                    )}
+                  />
+                  <p className="hidden lg:block">{name}</p>
+                </li>
+              </Link>
+            );
+          })}
+        </ul>
+      </nav>
+
+      <Image
+        src="/assets/images/files-2.png"
+        alt="logo"
+        width={506}
+        height={418}
+        className="w-full hidden lg:block"
+      />
+
+      <div className="mt-4 flex items-center justify-center gap-2 bg-blue-500 rounded-full  p-1 text-light-100 lg:justify-start lg:p-3">
+        <Image
+          src={avatarPlaceholderUrl}
+          alt="Avatar"
+          width={44}
+          height={44}
+          className=" aspect-square w-10 rounded-full object-cover"
+        />
+        <div className="hidden lg:block">
+          <p className="text-[16px] leading-[20px] font-semibold capitalize">
+            Kaji Azad Ali
+          </p>
+          <p className="text-[14px] leading-[16px] font-normal">
+            Contact@gamil.com
+          </p>
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
